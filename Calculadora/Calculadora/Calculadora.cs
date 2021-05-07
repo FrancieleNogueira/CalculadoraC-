97bedd59759a70fb621af18afcfc2ef7188d1651
Fran <Francarriel@gmail.com>
2021-05-07 10:14:24 -0300

    CalculadoraC#

---
+++ Calculadora/Calculadora/Calculadora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Calculadora
+    {
+        public static double Calcular(double valor1, char operador, double valor2)
+        {
+            switch (operador)
+            {
+                case '+':
+                    return valor1 + valor2;
+                case '-':
+                    return valor1 - valor2;
+                case '*':
+                    return valor1 * valor2;
+                case '/':
+                    return valor1 / valor2;
+                default:
+                    return 0.0;
+            }
+        }
+    }
+}
---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Header();
+
+            bool isSair = false;
+            try
+            {
+                while (!isSair)
+                {
+                    string entrada = Console.ReadLine();
+                    if (entrada == "SAIR" || entrada == "sair")
+                    {
+                        isSair = true;
+                    }
+                    else
+                    {
+                        string[] operacao = entrada.Split(' ');
+                        double valor1 = double.Parse(operacao[0], CultureInfo.InvariantCulture);
+                        char operador = char.Parse(operacao[1]);
+                        double valor2 = double.Parse(operacao[2], CultureInfo.InvariantCulture);
+
+                        //depois criar uma classe calculadora e criar os métodos de cada operação
+
+                        if (operador == '/' && valor2 == 0)
+                        {
+                            Console.WriteLine("Não é possível fazer divisão por zero");
+                        }
+                        else if (!(operador == '+' || operador == '-' || operador == '*' || operador == '/'))
+                        {
+                            Console.WriteLine("Entrada inválida, tente novamente");
+                        }
+                        else
+                        {
+                            double resultado = Calculadora.Calcular(valor1, operador, valor2);
+
+                            Console.WriteLine(resultado.ToString("F3", CultureInfo.InvariantCulture));
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Entrada inválida, tente novamente");
+            }
+            finally
+            {
+                Console.WriteLine("Obrigado por usar a calculadora.");
+            }
+        }
+
+        static void Header()
+        {
+
+            Console.WriteLine(" --------------------- CALCULADORA ---------------------");
+            Console.WriteLine("           Digite a operação no formato: ");
+            Console.WriteLine("            número  operador  número    ");
+            Console.WriteLine("--------------------------------------------------------");
+            Console.WriteLine("        Para sair digite SAIR ou sair ");
+            Console.WriteLine("--------------------------------------------------------");
+        }
+    }
+}
